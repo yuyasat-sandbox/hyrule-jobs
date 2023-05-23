17443d95ecab40d70d0d7b4a5f1a56aab3f8287b
yuyasat <takiyour@gmail.com>
2023-05-23 15:31:50 +0900

    style and images

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="app">
     <header>
+      <div class="title">
+        <img src="./assets/heart.svg" alt="site logo" />
+        <h1>Hyrule Jobs</h1>
+      </div>
       <div class="order">
         <button @click="handleClick('title')">order by title</button>
         <button @click="handleClick('salary')">order by salary</button>
@@ -84,5 +88,16 @@ button {
   border-radius: 4px;
   cursor: pointer;
   font-weight: bold;
+}
+header .title {
+  display: flex;
+  justify-content: center;
+}
+header img {
+  width: 60px;
+  margin-right: 20px;
+}
+header h1 {
+  font-size: 3em;
 }
 </style>
--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="job-list">
     <p>Ordered by {{ order }}</p>
-    <ul>
+    <transition-group name="list" tag="ul">
       <li v-for="job in orderedJobs" :key="job.id">
         <h2>{{ job.title }} in {{ job.location }}</h2>
         <div class="salary">
+          <img src="../assets/rupee.svg" alt="rupee icon" />
           <p>{{ job.salary }} rupees</p>
         </div>
         <div class="description">
@@ -14,7 +15,7 @@
           </p>
         </div>
       </li>
-    </ul>
+    </transition-group>
   </div>
 </template>
 <script lang="ts">
@@ -80,5 +81,9 @@ export default defineComponent({
   color: #17bf66;
   font-weight: bold;
   margin: 10px 4px;
+}
+
+.list-move {
+  transition: all 1s;
 }
 </style>
