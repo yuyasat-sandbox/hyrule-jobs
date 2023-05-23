0e4488c59fdfd2744a089412527e987ef692b518
yuyasat <takiyour@gmail.com>
2023-05-23 14:49:07 +0900

    introduce JobList

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,54 @@
 <template>
-  <div class="app">Hello, hyrule</div>
+  <div class="app"><JobList :jobs="jobs" /></div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, reactive, ref, toRefs } from "vue";
+import JobList from "./components/JobList.vue";
+import Job from "./types/Job";
 
 export default defineComponent({
   name: "App",
-  components: {},
+  components: {
+    JobList,
+  },
+  setup() {
+    const jobs = ref<Job[]>([
+      {
+        title: "farm worker",
+        location: "lon lon ranch",
+        salary: 30000,
+        id: "1",
+      },
+      {
+        title: "quarryman",
+        location: "death mountain",
+        salary: 40000,
+        id: "2",
+      },
+
+      {
+        title: "flute player",
+        location: "the lost woods",
+        salary: 35000,
+        id: "3",
+      },
+      {
+        title: "fishman",
+        location: "lake hylia",
+        salary: 21000,
+        id: "4",
+      },
+      {
+        title: "prison guard",
+        location: "gerud valley",
+        salary: 32000,
+        id: "5",
+      },
+    ]);
+    return { jobs };
+  },
+  methods: {},
 });
 </script>
 
